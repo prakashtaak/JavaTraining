92a9951a1ec2616c93e4716cfd8938f83d069be7
prakash <prakash@gmail.com>
2020-12-10 18:34:54 +0530

    symmetryencryption

--- src/main/java/com/encrypt/AES.java
+++ src/main/java/com/encrypt/AES.java
@@ -61,8 +61,6 @@ public class AES {
         String originalString = "howtodoinjava.com";
         String encryptedString = AES.encrypt(originalString, secretKey) ;
         String decryptedString = AES.decrypt(encryptedString, secretKey) ;
-
-
         System.out.println(decryptedString);
     }
 }
